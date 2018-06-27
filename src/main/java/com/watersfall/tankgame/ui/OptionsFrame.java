dcc324a5e6bd9f2e44b11d68de6d7fc7a0c8498a
Watersfall <dontshoot262@yahoo.com>
2018-06-26 20:59:07 -0400

    Fix visual bugs in the options menu

--- src/main/java/com/watersfall/tankgame/ui/OptionsFrame.java
+++ src/main/java/com/watersfall/tankgame/ui/OptionsFrame.java
@@ -5,6 +5,7 @@ import com.watersfall.tankgame.Constants;
 import com.watersfall.tankgame.Main;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -42,19 +43,25 @@ public class OptionsFrame extends JFrame
     private JLabel sfxLabel;
     private JLabel musicLabel;
     private JPanel panel;
+    private JPanel panel2;
     private JButton exitButton;
     private JButton returnButton;
     private JButton settingsButton;
     private JButton quitButton;
+    private JButton menuButton;
+    private boolean init;
 
 
     public OptionsFrame() throws IOException
     {
         super();
         this.setSize(Constants.SCREENWIDTH, Constants.SCREENHEIGHT);
+        init = false;
         setExtendedState(JFrame.MAXIMIZED_BOTH);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.panel = new JPanel();
+        this.panel2 = new JPanel();
+
         musicLabel = new JLabel();
         sfxLabel = new JLabel();
         musicSlider = new JSlider();
@@ -63,6 +70,7 @@ public class OptionsFrame extends JFrame
         returnButton = new JButton();
         settingsButton = new JButton();
         quitButton = new JButton();
+        menuButton = new JButton();
         configFolder.mkdirs();
 
         if(configFile.createNewFile())
@@ -86,6 +94,9 @@ public class OptionsFrame extends JFrame
         }
 
         panel.setLayout(null);
+        panel.setSize(Constants.SCREENWIDTH, Constants.SCREENHEIGHT);
+        panel2.setLayout(null);
+        panel2.setSize(Constants.SCREENWIDTH, Constants.SCREENHEIGHT);
         musicLabel.setText("Music Volume");
         sfxLabel.setText("Effects Volume");
         musicSlider.setMaximum(100);
@@ -101,6 +112,7 @@ public class OptionsFrame extends JFrame
             public void stateChanged(ChangeEvent e) {
                 musicVolume = musicSlider.getValue();
                 Main.sound.setMusicVolume((float)musicVolume);
+                repaint();
             }
         });
 
@@ -109,6 +121,7 @@ public class OptionsFrame extends JFrame
             public void stateChanged(ChangeEvent e) {
                 effectsVolume = sfxSlider.getValue();
                 Main.sound.setEffectsVolume((float)effectsVolume);
+                repaint();
             }
         });
 
@@ -122,14 +135,11 @@ public class OptionsFrame extends JFrame
                     fileWriter.newLine();
                     fileWriter.append("effectsVolume=" + effectsVolume);
                     fileWriter.close();
-                    quitButton.setVisible(true);
-                    returnButton.setVisible(true);
-                    settingsButton.setVisible(true);
-                    musicLabel.setVisible(false);
-                    musicSlider.setVisible(false);
-                    sfxLabel.setVisible(false);
-                    sfxSlider.setVisible(false);
-                    exitButton.setVisible(false);
+                    getContentPane().remove(panel2);
+                    
+                    getContentPane().add(panel);
+                    getContentPane().repaint();
+                    panel.repaint();
                 }
                 catch(IOException f)
                 {
@@ -156,29 +166,37 @@ public class OptionsFrame extends JFrame
         settingsButton.addActionListener(new ActionListener(){
             @Override
             public void actionPerformed(ActionEvent e) {
-                musicLabel.setVisible(true);
-                musicSlider.setVisible(true);
-                sfxLabel.setVisible(true);
-                sfxSlider.setVisible(true);
-                exitButton.setVisible(true);
-                quitButton.setVisible(false);
-                returnButton.setVisible(false);
-                settingsButton.setVisible(false);
+                getContentPane().remove(panel);
+                
+                getContentPane().add(panel2);
+                getContentPane().repaint();
             }
         });
 
-        panel.add(musicLabel);
-        panel.add(musicSlider);
-        panel.add(sfxLabel);
-        panel.add(sfxSlider);
-        panel.add(exitButton);
+        menuButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Main.selectionFrame.setVisible(true);
+                Main.selectionFrame.frame.setVisible(false);
+                Main.selectionFrame.frame = null;
+                setVisible(false);
+            }
+        });
+
+        panel2.add(musicLabel);
+        panel2.add(musicSlider);
+        panel2.add(sfxLabel);
+        panel2.add(sfxSlider);
+        panel2.add(exitButton);
         panel.add(quitButton);
         panel.add(returnButton);
         panel.add(settingsButton);
+        panel.add(menuButton);
         setUndecorated(true);
         this.getContentPane().add(panel);
         this.setBackground(new Color(0, 0, 0, 0));
         panel.setBackground(new Color(127, 127, 127, 127));
+        panel2.setBackground(new Color(127, 127, 127, 127));
         this.setAlwaysOnTop(true);
         pack();
         setVisible(false);
@@ -188,6 +206,7 @@ public class OptionsFrame extends JFrame
         initSettings();
         Main.sound.setEffectsVolume((float)effectsVolume);
         Main.sound.setMusicVolume((float)musicVolume);
+        init = true;
     }
 
     private void initSettings()
@@ -198,7 +217,7 @@ public class OptionsFrame extends JFrame
         musicLabel.setSize(10 * Constants.FONTSIZE, 100);
 
         musicSlider.setFont(new Font("Arial", Font.PLAIN, Constants.FONTSIZE / 2)); 
-        musicSlider.setForeground(new Color(0, 0, 0, 255));
+        musicSlider.setForeground(new Color(0, 0, 0, 0));
         musicSlider.setBackground(new Color(0, 0, 0, 0));
         musicSlider.setLocation((int)(150 * Constants.SCALE_X), (int)(205 * Constants.SCALE_Y));
         musicSlider.setSize((int)(200 * Constants.SCALE_X), 100);
@@ -209,7 +228,7 @@ public class OptionsFrame extends JFrame
         sfxLabel.setSize(10 * Constants.FONTSIZE, 100);
 
         sfxSlider.setFont(new Font("Arial", Font.PLAIN, Constants.FONTSIZE / 2)); 
-        sfxSlider.setForeground(new Color(0, 0, 0, 255));
+        sfxSlider.setForeground(new Color(0, 0, 0, 0));
         sfxSlider.setBackground(new Color(0, 0, 0, 0));
         sfxSlider.setLocation((int)(150 * Constants.SCALE_X), (int)(355 * Constants.SCALE_Y));
         sfxSlider.setSize((int)(200 * Constants.SCALE_X), 100);
@@ -217,29 +236,28 @@ public class OptionsFrame extends JFrame
         exitButton.setFont(new Font("Arial", Font.PLAIN, Constants.FONTSIZE / 2));
         exitButton.setLocation((int)(1650 * Constants.SCALE_X), (int)(950 * Constants.SCALE_Y));
         exitButton.setSize((int)(200 * Constants.SCALE_X), (int)(50 * Constants.SCALE_Y));
-        
-        musicLabel.setVisible(false);
-        musicSlider.setVisible(false);
-        sfxLabel.setVisible(false);
-        sfxSlider.setVisible(false);
-        exitButton.setVisible(false);
     }
 
     public void initMenu()
     {
         returnButton.setFont(new Font("Arial", Font.PLAIN, Constants.FONTSIZE / 2)); 
         returnButton.setSize((int)(350 * Constants.SCALE_X), (int)(125 * Constants.SCALE_Y));
-        returnButton.setLocation((int)(1920 / 2 * Constants.SCALE_X - (returnButton.getWidth() / 2)), (int)(300 * Constants.SCALE_Y));
+        returnButton.setLocation((int)(1920 / 2 * Constants.SCALE_X - (returnButton.getWidth() / 2)), (int)(200 * Constants.SCALE_Y));
         returnButton.setText("Return to Game");
 
         settingsButton.setFont(new Font("Arial", Font.PLAIN, Constants.FONTSIZE / 2)); 
         settingsButton.setSize((int)(350 * Constants.SCALE_X), (int)(125 * Constants.SCALE_Y));
-        settingsButton.setLocation((int)(1920 / 2 * Constants.SCALE_X - (settingsButton.getWidth() / 2)), (int)(500 * Constants.SCALE_Y));
+        settingsButton.setLocation((int)(1920 / 2 * Constants.SCALE_X - (settingsButton.getWidth() / 2)), (int)(400 * Constants.SCALE_Y));
         settingsButton.setText("Settings");
+
+        menuButton.setFont(new Font("Arial", Font.PLAIN, Constants.FONTSIZE / 2));
+        menuButton.setSize((int)(350 * Constants.SCALE_X), (int)(125 * Constants.SCALE_Y));
+        menuButton.setLocation((int)(1920 / 2 * Constants.SCALE_X - (menuButton.getWidth() / 2)), (int)(600 * Constants.SCALE_Y));
+        menuButton.setText("Return to Menu");
 
         quitButton.setFont(new Font("Arial", Font.PLAIN, Constants.FONTSIZE / 2)); 
         quitButton.setSize((int)(350 * Constants.SCALE_X), (int)(125 * Constants.SCALE_Y));
-        quitButton.setLocation((int)(1920 / 2 * Constants.SCALE_X - (quitButton.getWidth() / 2)), (int)(700 * Constants.SCALE_Y));
+        quitButton.setLocation((int)(1920 / 2 * Constants.SCALE_X - (quitButton.getWidth() / 2)), (int)(800 * Constants.SCALE_Y));
         quitButton.setText("Exit Game");
     }
 }
